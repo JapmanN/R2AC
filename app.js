09f65a98de5308385a4bb6e9b06ec7afd7df7ac0
JapmanN <japmannagra20@gmail.com>
2020-11-16 21:36:31 -0500

    added table

---
+++ app.js
@@ -0,0 +1,32 @@
+// ===============================
+//  REQUIRE ALL NECESSARY PACKAGES
+// ===============================
+const express = require("express"),
+app           = express(),
+api           = require('novelcovid');
+
+// =========
+// APP SETUP
+// =========
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+api.settings({
+    baseUrl: 'https://disease.sh'
+});
+
+// ==========
+// APP ROUTES
+// ==========
+app.get("/", (req, res) => {
+    api.countries({sort:'cases'}).then(function(countries) {
+        res.render("index", {countries: countries});
+    });
+});
+
+// ==============
+// SERVER STARTUP
+// ==============
+app.listen(process.env.PORT || 3000, (req, res) => {
+    console.log("server is running...")
+});
